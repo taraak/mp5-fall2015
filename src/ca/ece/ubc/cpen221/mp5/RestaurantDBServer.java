3acb55319d675af92a5d9d3f2e4e6efe3cff7731
Tara <tara.ak.sa@gmail.com>
2015-11-24 19:04:25 -0800

    added main in server

--- src/ca/ece/ubc/cpen221/mp5/RestaurantDBServer.java
+++ src/ca/ece/ubc/cpen221/mp5/RestaurantDBServer.java
@@ -4,6 +4,11 @@ package ca.ece.ubc.cpen221.mp5;
 // process queries concurrently, etc.
 
 public class RestaurantDBServer {
+    
+    private String port;
+    private String reviewsJSONfilename; 
+    private String restaurantJSONfilename; 
+    private String usersJSONfilename; 
 
 	/**
 	 * Constructor
@@ -13,9 +18,19 @@ public class RestaurantDBServer {
 	 * @param reviewsJSONfilename
 	 * @param usersJSONfilename
 	 */
-	public RestaurantDBServer(int port, String filename1, String filename2, String filename3) {
+	public RestaurantDBServer(int port, String restoFile, String reviewsFile, String usersFile) {
 		// TODO: See the problem statement for what the arguments are.
 		// TODO: Rename the arguments suitably.
+
+	}
+	
+	public void main(String[] args){
+	    this.port=args[0];
+	    this.restaurantJSONfilename=args[1];
+	    this.reviewsJSONfilename=args[2];
+	    this.usersJSONfilename=args[3];
+	    
+	    
 	}
 
 }
