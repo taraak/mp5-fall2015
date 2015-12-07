b5818f69bea5336e7b757be9d95a7478bd7a3464
Annabelle Harvey <annabelle.ahrv@gmail.com>
2015-12-06 17:15:27 -0800

    updating stat

---
+++ src/ca/ece/ubc/cpen221/mp5/statlearning/LatitudeFF.java
@@ -0,0 +1,14 @@
+package ca.ece.ubc.cpen221.mp5.statlearning;
+
+import ca.ece.ubc.cpen221.mp5.Restaurant;
+import ca.ece.ubc.cpen221.mp5.RestaurantDB;
+
+public class LatitudeFF implements MP5Function {
+
+    @Override
+    public double f(Restaurant yelpRestaurant, RestaurantDB db) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
---
+++ src/ca/ece/ubc/cpen221/mp5/statlearning/RatingFF.java
@@ -0,0 +1,5 @@
+package ca.ece.ubc.cpen221.mp5.statlearning;
+
+public class RatingFF implements MP5Function {
+
+}
